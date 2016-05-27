b256b080bcc7a108f0f89a8bde8ab96245bd16af
ersin-ertan <ersin_the_ertan@hotmail.com>
2016-05-27 15:40:53 +0700

    Added Backboard default examples, will add some personal examples later

---
+++ backboard/src/main/java/com/nullcognition/backboard/PressFragment.java
@@ -0,0 +1,28 @@
+package com.nullcognition.backboard;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Demonstrates a view that shrinks when touched and bounces back when released.
+ * <p/>
+ * Created by ericleong on 11/28/15.
+ */
+public class PressFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                         Bundle savedInstanceState) {
+
+		final View rootView = inflater.inflate(R.layout.fragment_press, container, false);
+
+//		new Actor.Builder(SpringSystem.create(), rootView.findViewById(R.id.circle))
+//				.addMotion(new ToggleImitator(null, 1.0, 0.5), View.SCALE_X, View.SCALE_Y)
+//				.build();
+
+		return rootView;
+	}
+}
